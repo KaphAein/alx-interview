b4e2f4b246037b0a146f2995f083f276560393a9
KaphAein <cherifafadaly@gmail.com>
2024-10-03 17:53:14 +0000

    commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Pascal Triangle Module'''
+
+
+def pascal_triangle(n):
+  '''Creats a Pascal Triangle'''
+  triangle = []
+  if not isinstance(n, int) or n <= 0:
+    return triangle
+     
+  for y in range(n):
+    row = [1]
+    
+    if (y > 0):
+      for x in range(1, y):
+        row.append(triangle[y-1][x-1] + triangle[y-1][x])
+      row.append(1)
+    triangle.append(row)
+  return triangle
