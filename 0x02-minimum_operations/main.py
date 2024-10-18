95d17a4c25702399c7a356a3aff6a2fbe8d3a9ac
KaphAein <cherifafadaly@gmail.com>
2024-10-18 11:36:06 +0000

    commit

--- 0x02-minimum_operations/main.py
+++ 0x02-minimum_operations/main.py
@@ -4,6 +4,6 @@ Main file for copy/paste algorithm
 """
 import sys
 
-minOperations = __import__('0-minoperations').minOperations
+minOperations = __import__('1-minoperations').minOperations
 
 print(minOperations(2147483640))
