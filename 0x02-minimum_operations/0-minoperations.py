ba9264738beea4dbd3a67a05dbc56584df8acb6c
KaphAein <cherifafadaly@gmail.com>
2024-10-18 11:39:18 +0000

    commit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,7 +3,7 @@
 
 
 def minOperations(n):
-    '''Calculates the minimum number of operations to get exactly n 'H' characters'''
+    '''Calculates the minimum number of operations'''
 
     if n <= 1:
         return 0
@@ -12,11 +12,9 @@ def minOperations(n):
     divisor = 2
 
     while n > 1:
-        # Keep dividing `n` by the smallest divisor until it no longer divides cleanly
         while n % divisor == 0:
             operations += divisor
             n //= divisor
-        # Move to the next possible divisor
         divisor += 1
 
     return operations
