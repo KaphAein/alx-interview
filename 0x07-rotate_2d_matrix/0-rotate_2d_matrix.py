af26023e70d7351fee33400a2e8a729b70613748
KaphAein <cherifafadaly@gmail.com>
2024-11-19 11:36:04 +0000

    commit

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,7 +11,7 @@ def rotate_2d_matrix(matrix):
 
     for i in range(n):
         for j in range(i + 1, n):
-            matrix[i][j], matrix[j][i] =  matrix[j][i], matrix[i][j]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     for row in matrix:
         row.reverse()
