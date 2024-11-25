5e5ef62c807a711cfacda48c7a070a380e8e0d3b
KaphAein <cherifafadaly@gmail.com>
2024-11-25 13:54:21 +0000

    commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,12 +4,12 @@
 
 def makeChange(coins, total):
     '''Making Change Function'''
+    if total <= 0:
+        return 0
+
     coins = sorted(coins, reverse=True)
     counter = 0
     for coin in coins:
-        while total >= coin:
-            total -= coin
-            counter += 1
-    if total > 0:
-        return -1
-    return counter
+        count, total = divmod(total, coin)
+        counter += count
+    return counter if total == 0 else -1
