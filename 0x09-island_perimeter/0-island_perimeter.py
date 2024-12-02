328a6246f769bf9c513394393c97c1a5be9766e1
KaphAein <cherifafadaly@gmail.com>
2024-12-02 14:11:27 +0000

    commit

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Island Perimeter Module'''
+
+
+def island_perimeter(grid):
+    '''Island Perimeter Function'''
+    if not grid or not isinstance(grid, list):
+        return 0
+
+    perimeter = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == 1:
+                perimeter += 4
+
+                if x > 0 and grid[x - 1][y]:
+                    perimeter -= 1
+                if x < len(grid) - 1 and grid[x + 1][y]:
+                    perimeter -= 1
+                if y > 0 and grid[x][y - 1]:
+                    perimeter -= 1
+                if y < len(grid[0]) and grid[x][y + 1]:
+                    perimeter -= 1
+    return perimeter
