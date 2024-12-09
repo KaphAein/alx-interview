433106527087b5419e776c1774c8e48e36be955d
KaphAein <cherifafadaly@gmail.com>
2024-12-09 21:07:30 +0000

    commit

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Prime game module."""
+
+def isWinner(x, nums):
+    """Determines the winner of a prime game session with `x` rounds."""
+    if x < 1 or not nums:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+    max_n = max(nums)
+
+    primes = [True] * (max_n + 1)
+    primes[0] = primes[1] = False
+
+    for i in range(2, int(max_n**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_n + 1, i):
+                primes[j] = False
+
+    prime_counts = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_counts[i] = prime_counts[i - 1] + (1 if primes[i] else 0)
+
+    for n in nums:
+        prime_count = prime_counts[n]
+        if prime_count % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
